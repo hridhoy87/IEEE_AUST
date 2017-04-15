fc801eaf97eb64f4d8677eecb8906ccce44eb892
Hridoy <m.asaduzzaman.29@gmail.co>
2017-04-15 16:00:02 +0600

    Initial commit

---
+++ app/src/main/java/com/hridoy/user/ieee_aust/StartingPoint.java
@@ -0,0 +1,13 @@
+package com.hridoy.user.ieee_aust;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartingPoint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_point);
+    }
+}
