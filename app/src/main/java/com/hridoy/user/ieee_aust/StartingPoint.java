b214ff1bd88993a8ebce2eecf9be4bebc965a28d
Hridoy <m.asaduzzaman.29@gmail.co>
2017-04-15 16:52:41 +0600

    Do Add a List and RecList Next

--- app/src/main/java/com/hridoy/user/ieee_aust/StartingPoint.java
+++ app/src/main/java/com/hridoy/user/ieee_aust/StartingPoint.java
@@ -1,13 +1,55 @@
 package com.hridoy.user.ieee_aust;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartingPoint extends AppCompatActivity {
 
+    MediaPlayer creed;
+    Button start,stop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_point);
+        initialize();
+        //playSong();
     }
+
+    private void initialize() {
+        start=(Button)findViewById(R.id.bStartSong);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSong();
+            }
+        });
+        stop=(Button)findViewById(R.id.bStopSong);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                creed.release();
+            }
+        });
+    }
+
+    private void playSong() {
+        creed= MediaPlayer.create(StartingPoint.this,R.raw.one_last_breath);
+        creed.start();
+    }
+    /*
+    @Override
+    protected void onPause() {
+        super.onPause();
+        creed.pause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        creed.release();
+    }
+    */
 }
